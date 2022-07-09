0cd3fe1a251db97dfb06e09dcd19c606fb61c7db
hleeminh <hleeminh2221>
2022-07-09 20:51:55 +0700

    09/07/2022

--- src/index.js
+++ src/index.js
@@ -8,22 +8,28 @@ const path = require('path');
 
 app.use(express.static('./src/public'));
 
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 app.engine('.hbs', hbs.engine({
     extname: '.hbs'
 }));
 app.set('view engine', '.hbs');
-app.set('views', './src/resources/views')
+app.set('views', './src/resources/views');
 
 app.get('/', (req, res) => {
     res.render('home');
-})
+});
 
-app.get('/news', (req, res) => {
-    res.render('news');
-})
+app.get('/upload-file', (req, res) => {
+    res.render('upload-file');
+});
+
+app.get('/import-link', (req, res) => {
+    console.log(req.query.link);
+    res.render('import-link');
+
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
-})
+});
