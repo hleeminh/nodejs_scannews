32676269beeec7f9f692491b1ba5c74f95da83ff
hleeminh <hleeminh2221>
2022-07-20 16:18:20 +0700

    20/07/2022

--- src/app/models/Article.js
+++ src/app/models/Article.js
@@ -1,10 +1,17 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const moment = require('moment');
+const customDate = moment().format('dddd HH:mm:ss DD/MM/YYYY');
+mongoose.Schema.Types.customDate = customDate;
+
 
 const Article = new Schema({
     url: { type: String, maxLength: 255 },
-    createAt: { type: Date, default: Date.now },
-    updateAt: { type: Date, default: Date.now },
+    createAt: { type: String, default: customDate },
+    updateAt: { type: String, default: customDate },
+    // createAt: { type: Date, default: Date.now },
+    // updateAt: { type: Date, default: Date.now },
+
     status: { type: String, maxLength: 255 },
 });
 
